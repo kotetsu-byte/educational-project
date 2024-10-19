26d3608a58e846d6cba013cf438e0090e7ae2503
kotetsu-byte <gross.23.29.11@gmail.com>
2024-10-19 10:04:22 +0500

    10/19/2024 #2

--- uranus/courses/models.py
+++ uranus/courses/models.py
@@ -3,4 +3,4 @@ from django.db import models
 # Create your models here.
 class Course(models.Model):
     name = models.TextField(null=True)
-    description = models.TextField(null=True, max_length=255)
+    description = models.TextField(null=True)
--- uranus/lessons/models.py
+++ uranus/lessons/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Lesson(models.Model):
-    title = models.TextField(null=True, max_length=255)
+    title = models.TextField(null=True)
     info = models.TextField(null=True)
     course = models.ForeignKey('courses.Course', on_delete=models.CASCADE, null=True)
