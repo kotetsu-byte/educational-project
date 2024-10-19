4b401302b2a3469f369deb30c9924ba3b82ac50c
kotetsu-byte <gross.23.29.11@gmail.com>
2024-10-19 18:13:55 +0500

    10/19/2023 #3

--- uranus/tests/models.py
+++ uranus/tests/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Test(models.Model):
-    question = models.TextField(null=True, max_length=255)
-    answer1 = models.TextField(null=True, max_length=255)
-    answer2 = models.TextField(null=True, max_length=255)
-    answer3 = models.TextField(null=True, max_length=255)
-    answer4 = models.TextField(null=True, max_length=255)
+    question = models.TextField(null=True)
+    answer1 = models.TextField(null=True)
+    answer2 = models.TextField(null=True)
+    answer3 = models.TextField(null=True)
+    answer4 = models.TextField(null=True)
     correct_answer = models.IntegerField(null=True)
     points = models.IntegerField(null=True)
     course = models.ForeignKey('courses.Course', on_delete=models.CASCADE, null=True)
