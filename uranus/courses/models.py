ade6cfec8909ae838fd1dd51c880c54e77be6f4f
kotetsu-byte <gross.23.29.11@gmail.com>
2024-10-19 08:55:27 +0500

    "10/19/2024"

--- uranus/courses/models.py
+++ uranus/courses/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 # Create your models here.
 class Course(models.Model):
-    name = models.CharField(null=True, max_length=255)
+    name = models.TextField(null=True)
     description = models.TextField(null=True, max_length=255)
--- uranus/tests/models.py
+++ uranus/tests/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Test(models.Model):
-    question = models.CharField(null=True, max_length=255)
-    answer1 = models.CharField(null=True, max_length=255)
-    answer2 = models.CharField(null=True, max_length=255)
-    answer3 = models.CharField(null=True, max_length=255)
-    answer4 = models.CharField(null=True, max_length=255)
+    question = models.TextField(null=True, max_length=255)
+    answer1 = models.TextField(null=True, max_length=255)
+    answer2 = models.TextField(null=True, max_length=255)
+    answer3 = models.TextField(null=True, max_length=255)
+    answer4 = models.TextField(null=True, max_length=255)
     correct_answer = models.IntegerField(null=True)
     points = models.IntegerField(null=True)
     course = models.ForeignKey('courses.Course', on_delete=models.CASCADE, null=True)
